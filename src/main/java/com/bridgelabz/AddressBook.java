315166ba7661c81fd3020670381735c7f2c8e142
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-24 15:25:11 +0530

    MODIFIED: add contact in address book

---
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class AddressBook {
+
+    Contact contact;
+
+    public void add() {
+        contact = new Contact();
+        contact.setFirstName("abc");
+        contact.setLastName("def");
+        contact.setAddress("hol");
+        contact.setCity("fjk");
+        contact.setState("bhu");
+        contact.setZip("yuj");
+        contact.setPhoneNum("45468446");
+        contact.setEmail("abc@gmail.com");
+    }
+}
--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,8 +1,10 @@
 package com.bridgelabz;
 
-public class AdressBook
+public class AddressBookMain
 {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook = new AddressBook();
+        addressBook.add();
     }
 }
