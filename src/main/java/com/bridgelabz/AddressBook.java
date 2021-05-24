a82a47997f7b03007dcfcacba55ee434d255c1d2
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-24 16:26:46 +0530

    MODIFIED: fuctionality to add new contact

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,18 +1,37 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
     Contact contact;
+    Scanner scanner = new Scanner(System.in);
 
     public void add() {
         contact = new Contact();
-        contact.setFirstName("abc");
-        contact.setLastName("def");
-        contact.setAddress("hol");
-        contact.setCity("fjk");
-        contact.setState("bhu");
-        contact.setZip("yuj");
-        contact.setPhoneNum("45468446");
-        contact.setEmail("abc@gmail.com");
+        System.out.println("Enter first name");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter address");
+        String address = scanner.nextLine();
+        System.out.println("Enter city");
+        String city = scanner.nextLine();
+        System.out.println("Enter state");
+        String state = scanner.nextLine();
+        System.out.println("Enter zip number");
+        String zip = scanner.nextLine();
+        System.out.println("Enter phone number");
+        String phoneNum = scanner.nextLine();
+        System.out.println("Enter email");
+        String email = scanner.nextLine();
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhoneNum(phoneNum);
+        contact.setEmail(email);
     }
 }
