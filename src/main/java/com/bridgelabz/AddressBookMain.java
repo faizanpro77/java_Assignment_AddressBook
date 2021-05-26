b0c3e48db860d30d50bcf1d199a014cb01523e18
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-27 00:16:00 +0530

    MODIFIED: add switch case in AddressBookMain

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,12 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain
 {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
         AddressBook addressBook = new AddressBook();
-        addressBook.add();
-        System.out.println(addressBook.edit());
-        addressBook.delete();
+        Scanner option = new Scanner(System.in);
+        System.out.println("Your choice for contact");
+        System.out.println("1 for add");
+        System.out.println("2 for edit");
+        System.out.println("3 for delete");
+        int choice = option.nextInt();
+        switch (choice) {
+            case 1:
+                addressBook.add();
+                break;
+            case 2:
+                addressBook.edit();
+                break;
+            case 3:
+                addressBook.delete();
+        }
     }
 }
